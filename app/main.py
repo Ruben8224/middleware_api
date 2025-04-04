9c376aafd09a65be3ca8c2aad0e1ade1697dff1a
Ruben8224 <tartas_casr@hotmail.com>
2025-04-04 11:56:45 -0600

    cambios

--- app/main.py
+++ app/main.py
@@ -9,14 +9,15 @@ app = FastAPI()
 
 os.makedirs("reportes", exist_ok=True)
 
+app.mount("/reportes", StaticFiles(directory="reportes"), name="reportes")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  # Frontend Angular
+    allow_origins=["*"],  # Puedes poner el dominio exacto de tu Angular si ya lo sabes
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.mount("/reportes", StaticFiles(directory="reportes"), name="reportes")
-
+# Incluir rutas
 app.include_router(router)
