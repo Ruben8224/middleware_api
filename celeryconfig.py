1f2856cc99865446829b1157b341cbfdb35fc937
Ruben8224 <tartas_casr@hotmail.com>
2025-04-04 15:14:49 -0600

    master

--- celeryconfig.py
+++ celeryconfig.py
@@ -5,6 +5,15 @@ load_dotenv()
 
 broker_url = os.getenv("REDIS_URL")
 result_backend = os.getenv("REDIS_URL")
-task_serielizer = 'json'
+
+task_serializer = 'json'
 accept_content = ['json']
-timezoe = 'UTC'
+timezone = 'UTC'
+
+# Aiven usa rediss:// (SSL), se requiere esta config
+broker_use_ssl = {
+    "ssl_cert_reqs": "none"
+}
+result_backend_use_ssl = {
+    "ssl_cert_reqs": "none"
+}
